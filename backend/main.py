e485dcdc5887ab9ea15075e1cbd1d3a98c96df13
Charlie Pyle <charlierpyle@gmail.com>
2026-03-12 21:46:47 -0500

    Created implementation for the fast api to run in python

--- backend/main.py
+++ backend/main.py
@@ -3,3 +3,12 @@ import PyPDF2
 from fastapi import FastAPI, UploadFile, File
 from fastapi.middleware.cors import CORSMiddleware
 
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
