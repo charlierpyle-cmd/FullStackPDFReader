f88e3b1ffc9a67b2cf8526f02e892717403948b5
Charlie Pyle <charlierpyle@gmail.com>
2026-03-13 22:23:08 -0500

    created method to list voices and full them from the database

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,7 @@ import pyttsx3
 import PyPDF2
 from fastapi import FastAPI, UploadFile, File
 from fastapi.middleware.cors import CORSMiddleware
-from database import init_db
+from database import init_db, save_voices
 
 app = FastAPI()
 
@@ -15,3 +15,10 @@ app.add_middleware(
 
 init_db()
 
+def list_voices():
+    engine = pyttsx3.init()
+    voices = engine.getProperty('voices')
+    voice_list = [{"id": v.id, "name": v.name} for v in voices]
+    engine.stop()
+    save_voices(voice_list)
+    return voice_list
